48039fa6dd3b35671337dbdad44033883ed5da26
Peter Parker <thivvnlh@gmail.com>
2020-05-26 08:27:36 +0700

    add UI Admin

---
+++ admin/js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$("#table").dataTable({
+		"searching": false
+	})
+});
+
+$('.collapse-link.DS').on("click", function () {
+	$(".x_content.DS").toggle("fast");
+})
+
+$('.collapse-link.BL').on("click", function () {
+	$(".x_content.BL").toggle("fast");
+})
+
+$('#menu_toggle').on("click", function () {
+	console.log("click")
+	if ($('footer').css("margin-left") === "0px") {
+		$('.top_nav').css("margin-left", "230px")
+		$('.right_col').css("margin-left", "230px")
+		$('.col-md-3.left_col').css("left", "0")
+		$('footer').css("margin-left", "230px")
+		$('.nav-md .container.body .col-md-3.left_col').css('display', 'flex')
+	} else {
+		$('.top_nav').css("margin-left", "0px")
+		$('.right_col').css("margin-left", "0px")
+		$('footer').css("margin-left", "0px")
+		$('.nav-md .container.body .col-md-3.left_col').css('display', 'none')
+	}
+})
